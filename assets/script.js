f7ff2a100e2ac48daaf86d0d447b1966059b6c81
faithkibuna <faithkibuna9@gmail.com>
2025-12-06 02:50:26 +0300

    changes

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+let age = 23;
+let name = "faith";
+console.log("My name is " + name + " and I am " + age + " years old ");
+
+age = age + 10;
+console.log("In 10 years, I will be " + age + " years old ");
